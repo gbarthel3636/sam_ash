60e234316be1c7eb4687bf2167b6e61a78489821
gbarthel3636 <george.barthel@powerhrg.com>
2019-04-08 12:54:17 -0400

    Add new create project

--- lib/sam_ash.rb
+++
@@ -1,6 +0,0 @@
-require "sam_ash/version"
-
-module SamAsh
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- spec/sam_ash_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe SamAsh do
-  it "has a version number" do
-    expect(SamAsh::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
